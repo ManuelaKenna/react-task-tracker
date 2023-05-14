cb56cf5db7b9beea4a07390490dce48e418d475c
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2023-05-13 18:38:32 -0700

    added toggle and click events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {useState} from 'react'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from './components/AddTask'
 
 
   const App = () => {
+    const [showAddTask, setShowAddTask] = useState
+    (false)
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,6 +29,13 @@ import Tasks from "./components/Tasks"
     }
 ])
 
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id)) 
@@ -41,7 +52,8 @@ import Tasks from "./components/Tasks"
 
   return (
     <div className="container">
-     <Header />
+     <Header onAdd={() => setShowAddTask(!showAddTask)} />
+     {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} 
      onDelete={deleteTask} onToggle={toggleReminder}/>
